2294f6f8a814f812768b001a7edbca49022f765c
clever-junior <tk_clever.junior@outlook.com>
2023-03-07 12:10:27 -0400

    feat: add requisite 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,12 @@
+# import datetime
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+  query = {"title": {"$regex": title, "$options": "i"}}
 
+  return [(news["title"], news["url"]) for news in search_news(query)]
 
 # Requisito 8
 def search_by_date(date):
