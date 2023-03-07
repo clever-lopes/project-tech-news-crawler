105eb875b29a62f0eacaf7493829f7c4df822768
clever-junior <tk_clever.junior@outlook.com>
2023-03-07 12:02:24 -0400

    feat: add requisite 6

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,7 +1,12 @@
+import pytest
+
 from tech_news.analyzer.reading_plan import (
     ReadingPlanService,
 )  # noqa: F401, E261, E501
 
 
 def test_reading_plan_group_news():
-    pass
+    with pytest.raises(
+        ValueError, match="Valor 'available_time' deve ser maior que zero"
+    ):
+        ReadingPlanService.group_news_for_available_time(0)
