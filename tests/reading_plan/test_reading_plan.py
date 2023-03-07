ddb2eb09b36ba22775bfe67f213cd3600bf371e5
clever-junior <tk_clever.junior@outlook.com>
2023-03-07 12:46:19 -0400

    fix: test

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -9,4 +9,4 @@ def test_reading_plan_group_news():
     with pytest.raises(
         ValueError, match="Valor 'available_time' deve ser maior que zero"
     ):
-        ReadingPlanService.group_news_for_available_time(0)
+        ReadingPlanService.group_news_for_available_time(-1)
