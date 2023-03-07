69f1d8d2a2b02f79784ae86f498afb96dab8149c
clever-junior <tk_clever.junior@outlook.com>
2023-03-07 12:26:08 -0400

    feat: requisite 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,14 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    data = find_news()
+    category_list = [news["category"] for news in data]
+
+    counter = Counter(category_list)
+    categories = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+
+    result = [category[0] for category in categories]
+    return result[:5]
